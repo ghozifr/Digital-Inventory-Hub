c0087708a76636d08e0e16e6deb85eef9cf4dcfd
ghozifr <ghozifr@gmail.com>
2024-04-09 18:58:23 +0700

    continuing add product

--- inventory_hub/lib/app/modules/add_products/controllers/add_products_controller.dart
+++ inventory_hub/lib/app/modules/add_products/controllers/add_products_controller.dart
@@ -2,4 +2,20 @@ import 'package:get/get.dart';
 
 class AddProductsController extends GetxController {
   RxBool isLoading = false.obs;
+
+  Future<Map<String, dynamic>> addProduct(Map<String, dynamic> data) async {
+    try {
+      //
+      return {
+        "error": false,
+        "message": "can add product",
+      };
+    } catch (e) {
+      //general error
+      return {
+        "error": true,
+        "message": "cannot add product",
+      };
+    }
+  }
 }
