34c1d8c5eb9d2698ace9bf77a692b2d721c4d0d2
Alfikra Muhammad <alfikramuhammad11@gmail.com>
2024-05-01 19:47:23 +0800

    Add illustration

--- inventory_hub/lib/app/modules/login/views/login_view.dart
+++ inventory_hub/lib/app/modules/login/views/login_view.dart
@@ -16,91 +16,110 @@ class LoginView extends GetView<LoginController> {
   );
 
   final AuthController authC = Get.find<AuthController>();
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('LOGIN'),
-        centerTitle: true,
+ @override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text('Hi! Welcome back', style: TextStyle(
+        color: Color(0xFF3383C5),
+        fontWeight: FontWeight.bold,
+        ),
       ),
-      body: ListView(
-        padding: const EdgeInsets.all(20),
-        children: [
-          TextField(
-            autocorrect: false,
-            controller: emailC,
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
-              labelText: "Email",
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(9),
-              ),
-            ),
-          ),
-          const SizedBox(height: 20),
-          Obx(
-            () => TextField(
+      centerTitle: true,
+      backgroundColor: Colors.white,
+    ),
+    
+    body: ListView(
+      padding: const EdgeInsets.all(20),
+      children: [
+        // Illustration Picture
+        Image.asset(
+          'lib/assets/images/logo.png', // image path
+          width: 200, 
+          height: 200, 
+        ),
+        const SizedBox(height: 50),
+        // Email TextField
+        Column(
+          children: [
+            TextField(
               autocorrect: false,
-              controller: passC,
-              keyboardType: TextInputType.text,
-              obscureText: controller.isHidden.value,
+              controller: emailC,
+              keyboardType: TextInputType.emailAddress,
               decoration: InputDecoration(
-                labelText: "Password",
-                suffixIcon: IconButton(
-                  onPressed: () {
-                    controller.isHidden.toggle();
-                  },
-                  icon: Icon(
-                    controller.isHidden.isFalse
-                        ? Icons.remove_red_eye
-                        : Icons.remove_red_eye_outlined,
-                  ),
-                ),
+                labelText: "Email",
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(9),
                 ),
               ),
             ),
-          ),
-          const SizedBox(height: 15),
-          TextButton(
-            onPressed: () {
-              Get.toNamed(Routes.registration);
-            },
-            child: Text("Create an account"),
-          ),
-          const SizedBox(height: 35),
-          ElevatedButton(
-            onPressed: () async {
-              if (controller.isLoading.isFalse) {
-                if (emailC.text.isNotEmpty && passC.text.isNotEmpty) {
-                  controller.isLoading(true);
-                  Map<String, dynamic> hasil =
-                      await authC.login(emailC.text, passC.text);
-                  controller.isLoading(false);
-
-                  if (hasil["error"] == true) {
-                    Get.snackbar("Error", hasil["message"]);
-                  } else {
-                    Get.offAllNamed(Routes.home);
-                  }
-                } else {
-                  Get.snackbar("Error", "Email dan password must be fill.");
-                }
-              }
-            },
-            style: ElevatedButton.styleFrom(
-              shape: RoundedRectangleBorder(
+            const SizedBox(height: 20),
+          ],
+        ),
+        // Password TextField
+        Obx(
+          () => TextField(
+            autocorrect: false,
+            controller: passC,
+            keyboardType: TextInputType.text,
+            obscureText: controller.isHidden.value,
+            decoration: InputDecoration(
+              labelText: "Password",
+              suffixIcon: IconButton(
+                onPressed: () {
+                  controller.isHidden.toggle();
+                },
+                icon: Icon(
+                  controller.isHidden.isFalse
+                      ? Icons.remove_red_eye
+                      : Icons.remove_red_eye_outlined,
+                ),
+              ),
+              border: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(9),
               ),
-              padding: const EdgeInsets.symmetric(vertical: 20),
-            ),
-            child: Obx(
-              () => Text(controller.isLoading.isFalse ? "LOGIN" : "LOADING..."),
             ),
           ),
-        ],
-      ),
-    );
-  }
+        ),
+        const SizedBox(height: 15),
+        TextButton(
+          onPressed: () {
+            Get.toNamed(Routes.registration);
+          },
+          child: Text("Create an account"),
+        ),
+        const SizedBox(height: 35),
+        ElevatedButton(
+          onPressed: () async {
+            if (controller.isLoading.isFalse) {
+              if (emailC.text.isNotEmpty && passC.text.isNotEmpty) {
+                controller.isLoading(true);
+                Map<String, dynamic> hasil =
+                    await authC.login(emailC.text, passC.text);
+                controller.isLoading(false);
+
+                if (hasil["error"] == true) {
+                  Get.snackbar("Error", hasil["message"]);
+                } else {
+                  Get.offAllNamed(Routes.home);
+                }
+              } else {
+                Get.snackbar("Error", "Email dan password must be filled.");
+              }
+            }
+          },
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(9),
+            ),
+            padding: const EdgeInsets.symmetric(vertical: 20),
+          ),
+          child: Obx(
+            () => Text(controller.isLoading.isFalse ? "LOGIN" : "LOADING..."),
+          ),
+        ),
+      ],
+    ),
+  );
+}
 }
