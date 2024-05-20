8c432141d2f7615e2103613c91f1b8eeb5a5cbfa
Alfikra Muhammad <alfikramuhammad11@gmail.com>
2024-05-20 15:55:26 +0800

    20/5

--- inventory_hub/lib/app/modules/detail_product/views/detail_product_view.dart
+++ inventory_hub/lib/app/modules/detail_product/views/detail_product_view.dart
@@ -17,180 +17,234 @@ class DetailProductView extends GetView<DetailProductController> {
 
   @override
   Widget build(BuildContext context) {
-    codeC.text = product.code;
-    nameC.text = product.name;
-    qtyC.text = "${product.qty}";
+  codeC.text = product.code;
+  nameC.text = product.name;
+  qtyC.text = "${product.qty}";
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('DETAIL PRODUCT', style: TextStyle(
-        color: Color(0xFF5B0888),
-        fontWeight: FontWeight.bold,
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text(
+        'DETAIL PRODUCT',
+        style: TextStyle(
+          color: Color(0xFF5B0888),
+          fontWeight: FontWeight.bold,
         ),
       ),
       backgroundColor: const Color(0XFFF8F9FF),
-        centerTitle: true,
-      ),
-      backgroundColor: const Color(0XFFF8F9FF),
-      body: ListView(
-        padding: const EdgeInsets.all(20),
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 200,
-                width: 200,
-                child: QrImage(
-                  data: product.code,
-                  size: 200.0,
-                  version: QrVersions.auto,
-                ),
+      centerTitle: true,
+    ),
+    backgroundColor: const Color(0XFFF8F9FF),
+    body: ListView(
+      padding: const EdgeInsets.all(20),
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 200,
+              width: 200,
+              child: QrImage(
+                data: product.code,
+                size: 200.0,
+                version: QrVersions.auto,
               ),
-            ],
+            ),
+          ],
+        ),
+        const SizedBox(height: 20),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(9),
+            boxShadow: [
+                    BoxShadow(
+                      color: const Color(0xFF0D2750).withOpacity(0.2),
+                      spreadRadius: 1,
+                      blurRadius: 8,
+                      offset: const Offset(6, 6), // changes position of shadow
+                    ),
+                    BoxShadow(
+                      color: const Color.fromARGB(255, 255, 255, 255).withOpacity(1),
+                      spreadRadius: 1,
+                      blurRadius: 8,
+                      offset: const Offset(-6, -6), // changes position of shadow
+                    ),
+                  ],
           ),
-          const SizedBox(height: 20),
-          TextField(
+          child: TextField(
             autocorrect: false,
             controller: codeC,
             keyboardType: TextInputType.number,
             readOnly: true,
             maxLength: 10,
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               labelText: "Product Code",
               filled: true,
               fillColor: Colors.white,
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(9),
-                borderSide: const BorderSide(
-                  color: Colors.red, // Change the color to your desired color
-                  width: 2.0, // Adjust the width if needed
-                ),
-              ),
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.all(15),
             ),
           ),
-          const SizedBox(height: 20),
-          TextField(
+        ),
+        const SizedBox(height: 20),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15),
+            boxShadow: [
+                    BoxShadow(
+                      color: const Color(0xFF0D2750).withOpacity(0.2),
+                      spreadRadius: 1,
+                      blurRadius: 8,
+                      offset: const Offset(6, 6), // changes position of shadow
+                    ),
+                    BoxShadow(
+                      color: const Color.fromARGB(255, 255, 255, 255).withOpacity(1),
+                      spreadRadius: 1,
+                      blurRadius: 8,
+                      offset: const Offset(-6, -6), // changes position of shadow
+                    ),
+                  ],
+          ),
+          child: TextField(
             autocorrect: false,
             controller: nameC,
             keyboardType: TextInputType.text,
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               labelText: "Product Name",
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(9),
-                borderSide: const BorderSide(
-                  color: Colors.red, // Change the color to your desired color
-                  width: 2.0, // Adjust the width if needed
-                ),
-              ),
+              filled: true,
+              fillColor: Colors.white,
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.all(15),
             ),
           ),
-          const SizedBox(height: 20),
-          TextField(
+        ),
+        const SizedBox(height: 20),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15),
+            boxShadow: [
+                    BoxShadow(
+                      color: const Color(0xFF0D2750).withOpacity(0.2),
+                      spreadRadius: 1,
+                      blurRadius: 8,
+                      offset: const Offset(6, 6), // changes position of shadow
+                    ),
+                    BoxShadow(
+                      color: const Color.fromARGB(255, 255, 255, 255).withOpacity(1),
+                      spreadRadius: 1,
+                      blurRadius: 8,
+                      offset: const Offset(-6, -6), // changes position of shadow
+                    ),
+                  ],
+          ),
+          child: TextField(
             autocorrect: false,
             controller: qtyC,
             keyboardType: TextInputType.number,
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               labelText: "Quantity",
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(9),
-                
-              ),
+              filled: true,
+              fillColor: Colors.white,
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.all(15),
             ),
           ),
-          const SizedBox(height: 35),
-          ElevatedButton(
-            onPressed: () async {
-              if (controller.isLoadingUpdate.isFalse) {
-                if (nameC.text.isNotEmpty && qtyC.text.isNotEmpty) {
-                  controller.isLoadingUpdate(true);
-                  Map<String, dynamic> hasil = await controller.editProduct({
-                    "id": product.productId,
-                    "name": nameC.text,
-                    "qty": int.tryParse(qtyC.text) ?? 0,
-                  });
-                  controller.isLoadingUpdate(false);
+        ),
+        const SizedBox(height: 35),
+        ElevatedButton(
+          onPressed: () async {
+            if (controller.isLoadingUpdate.isFalse) {
+              if (nameC.text.isNotEmpty && qtyC.text.isNotEmpty) {
+                controller.isLoadingUpdate(true);
+                Map<String, dynamic> hasil = await controller.editProduct({
+                  "id": product.productId,
+                  "name": nameC.text,
+                  "qty": int.tryParse(qtyC.text) ?? 0,
+                });
+                controller.isLoadingUpdate(false);
 
-                  Get.snackbar(
-                    hasil["error"] == true ? "Error" : "Success",
-                    hasil["message"],
-                    duration: const Duration(seconds: 2),
-                  );
-                } else {
-                  Get.snackbar(
-                    "Error",
-                    "All data must be filled in.",
-                    duration: const Duration(seconds: 2),
-                  );
-                }
+                Get.snackbar(
+                  hasil["error"] == true ? "Error" : "Success",
+                  hasil["message"],
+                  duration: const Duration(seconds: 2),
+                );
+              } else {
+                Get.snackbar(
+                  "Error",
+                  "All data must be filled in.",
+                  duration: const Duration(seconds: 2),
+                );
               }
-            },
-            style: ElevatedButton.styleFrom(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(9),
-              ),
-              padding: const EdgeInsets.symmetric(vertical: 20),
-              backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-              elevation: 5,
+            }
+          },
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(9),
             ),
-            child: Obx(
-              () => Text(controller.isLoadingUpdate.isFalse
-                  ? "UPDATE PRODUCT"
-                  : "LOADING..."),
-            ),
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+            elevation: 5,
           ),
-          TextButton(
-            onPressed: () {
-              Get.defaultDialog(
-                title: "Delete Product",
-                middleText: "Are you sure to delete this product ?",
-                actions: [
-                  OutlinedButton(
-                    onPressed: () => Get.back(),
-                    child: const Text("CANCEL"),
-                  ),
-                  ElevatedButton(
-                    onPressed: () async {
-                      controller.isLoadingDelete(true);
-                      Map<String, dynamic> hasil =
-                          await controller.deleteProduct(product.productId);
-                      controller.isLoadingDelete(false);
+          child: Obx(
+            () => Text(controller.isLoadingUpdate.isFalse
+                ? "UPDATE PRODUCT"
+                : "LOADING..."),
+          ),
+        ),
+        TextButton(
+          onPressed: () {
+            Get.defaultDialog(
+              title: "Delete Product",
+              middleText: "Are you sure to delete this product?",
+              actions: [
+                OutlinedButton(
+                  onPressed: () => Get.back(),
+                  child: const Text("CANCEL"),
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    controller.isLoadingDelete(true);
+                    Map<String, dynamic> hasil =
+                        await controller.deleteProduct(product.productId);
+                    controller.isLoadingDelete(false);
 
-                      Get.back(); // tutup dialog
-                      Get.back(); // Balik ke page all products
+                    Get.back(); // Close dialog
+                    Get.back(); // Navigate back to all products
 
-                      Get.snackbar(
-                        hasil["error"] == true ? "Error" : "Success",
-                        hasil["message"],
-                        duration: const Duration(seconds: 2),
-                      );
-                    },
-                    child: Obx(
-                      () => controller.isLoadingDelete.isFalse
-                          ? const Text("DELETE")
-                          : Container(
-                              padding: const EdgeInsets.all(2),
-                              height: 10,
-                              width: 15,
-                              child: const CircularProgressIndicator(
-                                color: Colors.white,
-                                strokeWidth: 1,
-                              ),
+                    Get.snackbar(
+                      hasil["error"] == true ? "Error" : "Success",
+                      hasil["message"],
+                      duration: const Duration(seconds: 2),
+                    );
+                  },
+                  child: Obx(
+                    () => controller.isLoadingDelete.isFalse
+                        ? const Text("DELETE")
+                        : Container(
+                            padding: const EdgeInsets.all(2),
+                            height: 10,
+                            width: 15,
+                            child: const CircularProgressIndicator(
+                              color: Colors.white,
+                              strokeWidth: 1,
                             ),
-                    ),
+                          ),
                   ),
-                ],
-              );
-            },
-            child: Text(
-              "Delete product",
-              style: TextStyle(
-                color: Colors.red.shade900,
-              ),
+                ),
+              ],
+            );
+          },
+          child: Text(
+            "Delete product",
+            style: TextStyle(
+              color: Colors.red.shade900,
             ),
           ),
-        ],
-      ),
-    );
-  }
+        ),
+      ],
+    ),
+  );
+}
 }
